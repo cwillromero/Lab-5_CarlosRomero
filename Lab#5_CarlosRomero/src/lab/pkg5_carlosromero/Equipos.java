354f659561ac67b60538895cf2632fe0a9a421cf
Will <Will@DESKTOP-E6E62EP>
2017-11-17 15:55:56 -0600

    Agreagr Modificar PopUp Menu

---
+++ Lab#5_CarlosRomero/src/lab/pkg5_carlosromero/Equipos.java
@@ -0,0 +1,68 @@
+
+package lab.pkg5_carlosromero;
+
+import java.util.ArrayList;
+
+public class Equipos {
+    public String Nombre;
+    public double Presupuesto;
+    public int CopasGanadas;
+    public String Estadio; 
+    public ArrayList<Jugadores> Jugadores=new ArrayList(); 
+
+    public Equipos() {
+    }
+
+    public Equipos(String Nombre, double Presupuesto, int CopasGanadas, String Estadio) {
+        this.Nombre = Nombre;
+        this.Presupuesto = Presupuesto;
+        this.CopasGanadas = CopasGanadas;
+        this.Estadio = Estadio;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public double getPresupuesto() {
+        return Presupuesto;
+    }
+
+    public void setPresupuesto(double Presupuesto) {
+        this.Presupuesto = Presupuesto;
+    }
+
+    public int getCopasGanadas() {
+        return CopasGanadas;
+    }
+
+    public void setCopasGanadas(int CopasGanadas) {
+        this.CopasGanadas = CopasGanadas;
+    }
+
+    public String getEstadio() {
+        return Estadio;
+    }
+
+    public void setEstadio(String Estadio) {
+        this.Estadio = Estadio;
+    }
+
+    public ArrayList<Jugadores> getJugadores() {
+        return Jugadores;
+    }
+
+    public void setJugadores(ArrayList<Jugadores> Jugadores) {
+        this.Jugadores = Jugadores;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre:  " + Nombre + "   CopasGanadas: " + CopasGanadas;
+    }
+       
+}
---
+++ Lab#5_CarlosRomero/src/lab/pkg5_carlosromero/Jugadores.java
@@ -0,0 +1,88 @@
+
+package lab.pkg5_carlosromero;
+
+public class Jugadores {
+    public String NombreJ;
+    public double Precio;
+    public String Posicion;
+    public boolean Disponibilidad;
+    public int Habilidad;
+    public int Tecnica;
+    public int Resistecia;
+
+    public Jugadores() {
+    }
+
+    public Jugadores(String NombreJ, double Precio, String Posicion, boolean Disponibilidad, int Habilidad, int Tecnica, int Resistecia) {
+        this.NombreJ = NombreJ;
+        this.Precio = Precio;
+        this.Posicion = Posicion;
+        this.Disponibilidad = Disponibilidad;
+        this.Habilidad = Habilidad;
+        this.Tecnica = Tecnica;
+        this.Resistecia = Resistecia;
+    }
+
+    public String getNombreJ() {
+        return NombreJ;
+    }
+
+    public void setNombreJ(String NombreJ) {
+        this.NombreJ = NombreJ;
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double Precio) {
+        this.Precio = Precio;
+    }
+
+    public String getPosicion() {
+        return Posicion;
+    }
+
+    public void setPosicion(String Posicion) {
+        this.Posicion = Posicion;
+    }
+
+    public boolean isDisponibilidad() {
+        return Disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean Disponibilidad) {
+        this.Disponibilidad = Disponibilidad;
+    }
+
+    public int getHabilidad() {
+        return Habilidad;
+    }
+
+    public void setHabilidad(int Habilidad) {
+        this.Habilidad = Habilidad;
+    }
+
+    public int getTecnica() {
+        return Tecnica;
+    }
+
+    public void setTecnica(int Tecnica) {
+        this.Tecnica = Tecnica;
+    }
+
+    public int getResistecia() {
+        return Resistecia;
+    }
+
+    public void setResistecia(int Resistecia) {
+        this.Resistecia = Resistecia;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + NombreJ + "   Precio: " + Precio + "   Posicion:" + Posicion + "   Disponibilidad: " + Disponibilidad;
+    }
+    
+    
+}
